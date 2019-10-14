d98ff18c16b6f5c5479dfe69227b35a58d09daa2
chill <chill@DESKTOP-H8TGFTS>
2019-10-14 13:14:45 +0200

    dwa

--- EjerciciosCondicionales/src/Ejercicio26.java
+++ EjerciciosCondicionales/src/Ejercicio26.java
@@ -3,20 +3,25 @@ import java.util.Scanner;
 public class Ejercicio26 {
 
 	public static void main(String[] args) {
-		int a, b, c, d;
+		double catetoOpuesto, catetoContiguo, hipotenusa, catetosSuma, hipotenusaCuadrado, casoDos, casoTres;
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Introduce el primer valor: ");
-		a = sc.nextInt();
-		System.out.print("Introduce el segundo valor: ");
-		b = sc.nextInt();
-		System.out.print("Introduce el tercer valor: ");
-		c = sc.nextInt();
-		a = a*a;
-		b = b*b;
-		c = c*c;
-		d = a+b;
-		if(c<d) {
-			System.out.println("");
+		System.out.print("Introduce el valor del cateto opuesto: ");
+		catetoOpuesto = sc.nextDouble();
+		catetoOpuesto = Math.pow(catetoOpuesto, 2);
+		System.out.print("Introduce el valor del cateto contiguo: ");
+		catetoContiguo = sc.nextDouble();
+		catetoContiguo = Math.pow(catetoContiguo, 2);
+		System.out.print("Introduce el valor de la hipotenusa: ");
+		hipotenusa = sc.nextDouble();
+		hipotenusaCuadrado = Math.pow(hipotenusa, 2);
+		catetosSuma = catetoOpuesto + catetoContiguo;
+		casoDos = hipotenusaCuadrado - catetoOpuesto;
+		casoTres = hipotenusaCuadrado - Math.pow(catetoContiguo, 2);
+		if(hipotenusaCuadrado == catetosSuma) {
+			System.out.println("Es un triangulo rectangulo");
+		}
+		else if(){
+			System.out.println("Es un triangulo obtusangulo");
 		}
 	}
 
