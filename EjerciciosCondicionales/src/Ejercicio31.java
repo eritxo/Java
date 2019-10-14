24f98be9c72f4367e200e375d90447c05b156f79
chill <chill@DESKTOP-H8TGFTS>
2019-10-14 15:00:57 +0200

    5

--- EjerciciosCondicionales/src/Ejercicio31.java
+++ EjerciciosCondicionales/src/Ejercicio31.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
 public class Ejercicio31 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double a, b, resultado;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ecuación de 1er grado : ax+b=0");
+		System.out.print("Introduce el valor de a: ");
+		a = sc.nextDouble();
+		System.out.print("Introduce el valor de b: ");
+		b = sc.nextDouble();
+		resultado = (-b)/a;
+		if((a == 0) && (b != 0)) {
+			System.out.println("No tiene solucion");
+		}
+		else if((a==0)&& (b==0)) {
+			System.out.println("Solucion indeterminada");
+		}
+		else {
+			System.out.println("La solucion es: "+ resultado);
+		}
 	}
 
 }
---
+++ EjerciciosCondicionales/src/Ejercicio32.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ejercicio32 {
+
+	public static void main(String[] args) {
+		double a, b, c, discriminante, resultadoPositivo, resultadoNegativo;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce el valor de a: ");
+		a = sc.nextDouble();
+		System.out.print("Introduce el valor de b: ");
+		b = sc.nextDouble();
+		System.out.print("Introduce el valor de c: ");
+		c = sc.nextDouble();
+		discriminante = Math.sqrt(Math.pow(b, 2) - (4*a*c));
+		resultadoPositivo = ((-b)+discriminante)/(2*a);
+		resultadoNegativo = ((-b)-discriminante)/(2*a);
+		if((a==0)) {
+			System.out.println("No es una ecuacion de segundo grado");
+		}
+		else if(discriminante < 0) {
+			System.out.println("No hay soluciones reales");
+		}
+		else if(discriminante == 0) {
+			System.out.println("Una unica solucion real");
+			System.out.println("x= "+ resultadoNegativo);
+
+		}
+		else if(discriminante > 0) {
+			System.out.println("x1= "+ resultadoNegativo);
+			System.out.println("x2= "+ resultadoPositivo);
+		}
+	}
+
+}
