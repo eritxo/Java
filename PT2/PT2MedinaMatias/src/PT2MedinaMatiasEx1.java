78c259eb59b83d4af1526008c6a48ba7911fb789
chill <chill@DESKTOP-H8TGFTS>
2019-11-28 13:03:10 +0100

    Udpate

--- PT2/PT2MedinaMatias/src/PT2MedinaMatiasEx1.java
+++ PT2/PT2MedinaMatias/src/PT2MedinaMatiasEx1.java
@@ -1,10 +1,48 @@
+import java.util.Scanner;
 
 public class PT2MedinaMatiasEx1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int
-
+		double temperaturaA=0, temperaturaB=0, temperaturaC=0, tempMedia, porcentajeMacho, 
+				porcentajeHembra, porcentajeNoFertil,totalHuevos;
+		int macho =0, hembra = 0, noFertil = 0, totalHuevosInt;
+		while((temperaturaA != -1) || (temperaturaB != -1) || (temperaturaC != -1)) {
+			System.out.print("Introduce la temperatura A: ");
+			temperaturaA = sc.nextDouble();
+			System.out.print("Introduce la temperatura B: ");
+			temperaturaB = sc.nextDouble();
+			System.out.print("Introduce la temperatura C: ");
+			temperaturaC = sc.nextDouble();
+			if((temperaturaA == -1) || (temperaturaB == -1) || (temperaturaC == -1)) {}
+			else {
+				if(((temperaturaA >= 28) && (temperaturaB >= 28) && (temperaturaC >= 28))&&
+						((temperaturaA <= 32) && (temperaturaB <= 32) && (temperaturaC <= 32))) {
+					tempMedia = (temperaturaA + temperaturaB + temperaturaC)/3;
+					if(tempMedia < 30) {
+						System.out.println("Macho");
+						macho++;
+					}
+					else {
+						System.out.println("Hembra");
+						hembra++;
+					}
+				}
+				else {
+					System.out.println("No fertil");
+					noFertil++;
+				}
+			}
+		}
+		totalHuevos = macho+hembra+noFertil;
+		totalHuevosInt = (int) totalHuevos;
+		porcentajeMacho = (macho*100)/totalHuevos;
+		porcentajeHembra = (hembra*100)/totalHuevos;
+		porcentajeNoFertil = (noFertil*100)/totalHuevos;
+		System.out.println("De un total de "+ totalHuevosInt +" huevos:");
+		System.out.println("Se ha registrado un "+ porcentajeMacho +"% de caimanes machos.");
+		System.out.println("Se ha registrado un "+ porcentajeHembra +"% de caimanes hembras.");
+		System.out.println("Se ha registrado un "+ porcentajeNoFertil +"% de huevos infertiles.");
 	}
 
 }
---
+++ PT2/PT2MedinaMatias/src/PT2MedinaMtiasEx2.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class PT2MedinaMtiasEx2 {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		double porcentajeAcietos;
+		int contador = 10, a, b, c, acietos;
+		while(contador > 0) {
+			a = random.nextInt(101);
+			b = random.nextInt(101);
+			
+			contador--;
+		}
+
+	}
+
+}
